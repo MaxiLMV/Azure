5d0d96a236769cb2ad3a8b9f433e27337e198fac
mfoltz <mitchell.foltz@gmail.com>
2024-04-04 22:26:50 -0500

    sync

--- Hooks/PetSystem.cs
+++ Hooks/PetSystem.cs
@@ -128,11 +128,12 @@ namespace VCreate.Hooks
 
             public static void UpdatePetLevelAndStats(PetExperienceProfile profile, Entity follower, Entity killer, Dictionary<string, PetExperienceProfile> profiles)
             {
+                /*
                 EntityCommandBufferSystem entityCommandBufferSystem = VWorld.Server.GetExistingSystem<EntityCommandBufferSystem>();
                 EntityCommandBuffer entityCommandBuffer = entityCommandBufferSystem.CreateCommandBuffer();
                 ServerGameManager serverGameManager = VWorld.Server.GetExistingSystem<ServerScriptMapper>()._ServerGameManager;
                 BuffUtility.BuffSpawner buffSpawner = BuffUtility.BuffSpawner.Create(serverGameManager);
-                profile.Level++;
+                
                 PrefabGUID bloodrage = new(-491593410);
                 PrefabGUID powersurge = new(-429891372);
                 PrefabGUID aegis = new(1050324275);
@@ -140,7 +141,17 @@ namespace VCreate.Hooks
                 BuffUtility.TryRemoveBuff(ref buffSpawner, entityCommandBuffer, bloodrage, follower);
                 BuffUtility.TryRemoveBuff(ref buffSpawner, entityCommandBuffer, powersurge, follower);
                 BuffUtility.TryRemoveBuff(ref buffSpawner, entityCommandBuffer, aegis, follower);
-                //var buffs = follower.ReadBuffer<BuffBuffer>();
+                */
+
+                profile.Level++;
+                var buffs = follower.ReadBuffer<BuffBuffer>();
+                foreach(var buff in buffs)
+                {
+                    if (buff.PrefabGuid.GuidHash == -491593410 || buff.PrefabGuid.GuidHash == -429891372 || buff.PrefabGuid.GuidHash == 1050324275)
+                    {
+                        SystemPatchUtil.Destroy(buff.Entity);
+                    }
+                }
                 Plugin.Log.LogInfo("Pet level up! Saving stats.");
                 follower.Write<UnitLevel>(new UnitLevel { Level = profile.Level });
                 UnitStatSet(follower); 
